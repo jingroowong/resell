86b6c0bb3935cd06bca585158a2d93ab2be4000b
Joyce <joyceokx-wm20@student.tarc.edu.my>
2023-09-22 16:00:44 +0800

    Initial Commit

--- app/src/main/java/com/example/resell/MainActivity.kt
+++ app/src/main/java/com/example/resell/MainActivity.kt
@@ -3,14 +3,46 @@ package com.example.resell
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.resell.database.AppDatabase
+import com.example.resell.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFrament(OrderHistory())
 
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+//                R.id.home -> replaceFrament(Home())
+//                R.id.cart -> replaceFrament(Cart())
+                R.id.history -> replaceFrament(OrderHistory())
+//                R.id.profile -> replaceFrament(Profile())
+
+                else ->{
+
+                }
+            }
+            true
+        }
     }
+
+    private fun replaceFrament(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+    }
+
+//    private  fun loadFragment(fragment: Fragment){
+//        val transaction = supportFragmentManager.beginTransaction()
+//        transaction.replace(R.id.fragment_container,fragment)
+//        transaction.commit()
+//    }
 
 }
