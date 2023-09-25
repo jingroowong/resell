68d3e3aac516dc3b9d2c8f1774cb4a3e04f602c9
Ming Zhe <nmingzhe02@gmail.com>
2023-09-25 16:16:46 +0800

    added single

--- app/src/main/java/com/example/resell/admin/ProductAdapter.kt
+++ app/src/main/java/com/example/resell/admin/ProductAdapter.kt
@@ -1,14 +1,49 @@
 package com.example.resell.admin
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.resell.R
 import com.example.resell.database.Product
+import com.example.resell.database.ProductDao
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adapter<ProductViewHolder>() {
+class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
 
+    class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(product: Product) {
+            val productNameTextView = itemView.findViewById<TextView>(R.id.product_name)
+            val productPriceTextView = itemView.findViewById<TextView>(R.id.product_price)
+            val productImageView = itemView.findViewById<ImageView>(R.id.product_image)
+            val productDateTextView = itemView.findViewById<TextView>(R.id.product_date)
+
+            productNameTextView.text = product.productName
+            productPriceTextView.text = "Price: RM${product.productPrice}"
+
+            val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+            val timestamp = product.dateUpload
+            if (timestamp != null) {
+                val date = Date(timestamp)
+                val formattedDate = dateFormat.format(date)
+                productDateTextView.text = formattedDate
+            }
+
+
+            Picasso.get()
+                .load(product.productImage) // Replace with your product's image URL field
+                .placeholder(R.drawable.ic_launcher_foreground) // Optional placeholder while loading
+                .error(R.drawable.ic_launcher_background) // Optional error image to display if loading fails
+                .into(productImageView)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
         val inflater = LayoutInflater.from(parent.context)
