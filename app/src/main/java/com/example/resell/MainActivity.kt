b199d4e811b8f264e1e8105b43b4ef8985b3395c
Mingzhe20 <nmingzhe02@gmail.com>
2023-10-01 20:08:12 +0800

    navigate history

--- app/src/main/java/com/example/resell/MainActivity.kt
+++ app/src/main/java/com/example/resell/MainActivity.kt
@@ -1,22 +1,45 @@
 package com.example.resell
 
 
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.google.firebase.FirebaseApp
+import androidx.navigation.fragment.findNavController
+import com.example.resell.admin.AdminViewProduct
+import com.example.resell.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        FirebaseApp.initializeApp(this)
-        val navController = this.findNavController(R.id.myNavHostFragment)
+        // Set up the bottom navigation view here
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.history -> findNavController(R.id.myNavHostFragment).navigate(R.id.action_adminViewProduct_to_orderHistory)
+                R.id.home -> findNavController(R.id.myNavHostFragment)
+                // Add other navigation items as needed
+                else -> {
+                }
+            }
+            true
+        }
 
-
+        // Set up navigation controller for fragments
+        val navController = findNavController(R.id.myNavHostFragment)
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
